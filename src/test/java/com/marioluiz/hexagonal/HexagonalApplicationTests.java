6dde340c27fa1b9b75ee05608c4ccd3cbc653779
MarioLuiz <marioluiz.arruda@gmail.com>
2024-07-17 11:32:46 -0400

    fix: arquivos iniciais

---
+++ src/test/java/com/marioluiz/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marioluiz.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
